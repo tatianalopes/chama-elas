2d4cc78f851aa52d1ce3fa2b4e3718f56129f2e6
tatianalopes <tcalopes19@gmail.com>
2020-12-17 22:08:48 -0300

    Implement JWT authentication

---
+++ backend/src/modules/user/services/AuthenticateUserService.ts
@@ -0,0 +1,57 @@
+import { sign } from 'jsonwebtoken';
+import { injectable, inject } from 'tsyringe';
+
+import autoConfig from '@config/auth';
+import AppError from '@shared/errors/AppError';
+import IHashProvider from '../providers/HashProvider/models/IHashProvider';
+import { User } from '../repositories/entities/User';
+import IUserRepository from '../repositories/IUserRepository';
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  user: User;
+  token: string;
+}
+
+@injectable()
+class AuthenticateUserService {
+  constructor(
+    @inject('UserRepository')
+    private userRepository: IUserRepository,
+
+    @inject('HashProvider')
+    private hashProvider: IHashProvider,
+  ) {}
+
+  public async execute({ email, password }: IRequest): Promise<IResponse> {
+    const user = await this.userRepository.findByEmail(email);
+    if (!user) {
+      throw new AppError('Incorrect email/password combination.', 401);
+    }
+
+    const passwordMatched = await this.hashProvider.compareHash(
+      password,
+      user.password,
+    );
+    if (!passwordMatched) {
+      throw new AppError('Incorrect email/password combination.', 401);
+    }
+
+    const { secret, expiresIn } = autoConfig.jwt;
+    const token = sign({}, secret, {
+      subject: user.id,
+      expiresIn,
+    });
+
+    return {
+      user,
+      token,
+    };
+  }
+}
+
+export default AuthenticateUserService;
--- backend/src/shared/routes.ts
+++ backend/src/shared/routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
 import userRouter from '@modules/user/routes/user.routes';
+import sessionRouter from '@modules/user/routes/session.routes';
 
 const routes = Router();
 
 routes.use('/user', userRouter);
+routes.use('/session', sessionRouter);
 
 export default routes;
